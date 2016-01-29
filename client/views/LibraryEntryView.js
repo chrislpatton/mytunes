76d21d81da0ae4a3c60dbae1787739b986aa9c32
chrislpatton <chrislpatton@gmail.com>
2016-01-28 23:21:08 -0500

    meets all basic requirement, but doesnt pass 2 tests

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -6,8 +6,7 @@ var LibraryEntryView = Backbone.View.extend({
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
 
   events: {
-    'click': function() {
-      this.model.play();
+    'click': function() {      
       this.model.enqueue();
     }
   },
